9241d39b8e42fcf48356230cea13f766684fde1b
bashayer <bisho5.ba@gmail.com>
2023-12-24 11:08:43 +0800

    Add the style sheet and add the lizard and spock choices to the game

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 let playerScore = 0
 let computerScore = 0
-
+let message = ''
 function getComputerChoice () {
-  const choices = ['rock', 'paper', 'scissors']
+  const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
   const randomChoice = Math.floor(Math.random() * choices.length)
   return choices[randomChoice]
 }
 
 function getPlayerInput () {
-  let playerInput = prompt('enter your choice')
-  return playerInput.toLowerCase()
+  const selectedRadio = document.querySelector('input[name="choice"]:checked')
+
+  // Get the value of the selected radio button
+  const selectedValue = selectedRadio.value
+  return selectedValue
 }
 
 function playRound (playerSelection, computerSelection) {
-  let message = ''
   if (playerSelection === computerSelection) {
-    message = 'TIE!'
+    message = "it's a TIE!"
   } else if (
     (playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'scissors' && computerSelection === 'rock') ||
-    (playerSelection === 'paper' && computerSelection === 'scissors')
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'lizard' && computerSelection === 'rock') ||
+    (playerSelection === 'spock' && computerSelection === 'lizard') ||
+    (playerSelection === 'scissors' && computerSelection === 'spock') ||
+    (playerSelection === 'lizard' && computerSelection === 'scissors') ||
+    (playerSelection === 'spock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'lizard') ||
+    (playerSelection === 'rock' && computerSelection === 'spock')
   ) {
     computerScore += 1
     message = `You Lose! ${computerSelection} beats ${playerSelection}`
   } else if (
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'rock' && computerSelection === 'lizard') ||
+    (playerSelection === 'lizard' && computerSelection === 'spock') ||
+    (playerSelection === 'spock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'lizard') ||
+    (playerSelection === 'paper' && computerSelection === 'spock') ||
+    (playerSelection === 'lizard' && computerSelection === 'paper') ||
+    (playerSelection === 'spock' && computerSelection === 'rock')
   ) {
     playerScore += 1
     message = `You Win! ${playerSelection} beats ${computerSelection}`
   }
-
-  return message
 }
 
 function game () {
@@ -51,4 +65,29 @@ function game () {
   }
 }
 
-game()
+const selectedRadio = document.querySelectorAll('input[name="choice"]')
+const playerScoreResult = document.querySelector('.player-score')
+const computerScoreResult = document.querySelector('.computer-score')
+const resultMsg = document.querySelector('.result-msg')
+
+selectedRadio.forEach(radio => {
+  radio.addEventListener('click', () => {
+    let computerSelection = getComputerChoice()
+    let playerSelection = getPlayerInput()
+    playRound(playerSelection, computerSelection)
+    playerScoreResult.textContent = playerScore
+    computerScoreResult.textContent = computerScore
+    resultMsg.textContent = message
+  })
+})
+
+const closeBtn = document.querySelector('.close-icon')
+const rulesBtn = document.querySelector('.rules-btn')
+const rulesDiv = document.querySelector('.rules-background')
+
+closeBtn.addEventListener('click', () => {
+  rulesDiv.classList.add('display-none')
+})
+rulesBtn.addEventListener('click', () => {
+  rulesDiv.classList.remove('display-none')
+})
